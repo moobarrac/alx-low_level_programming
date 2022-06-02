7f88b16f25578aceb144ef8aba496a2c4456d707
moobarrac <wahabajibola01@gmail.com>
2022-06-02 04:32:01 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(){
-	printf("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
