9ffe0223b0707f5beb74813b7f66eaaeb5ea7aad
moobarrac <wahabajibola01@gmail.com>
2022-06-03 03:39:54 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: a program that prints
+ * all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
