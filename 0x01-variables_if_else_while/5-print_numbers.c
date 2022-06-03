37ec5d11b28fa7240586340cedfc4f330902a76f
moobarrac <wahabajibola01@gmail.com>
2022-06-03 02:27:08 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description - Print numbers from 0 to 9
+ * Always return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
