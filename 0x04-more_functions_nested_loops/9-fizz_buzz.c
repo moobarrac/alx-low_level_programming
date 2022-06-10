63412d44d01fd261a9d7999cfd61bdc1d926e9f0
moobarrac <wahabajibola01@gmail.com>
2022-06-09 17:13:32 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,21 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= 100)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			print("%d ", i);
+			print("%d ",i);
+
+		if (i != 100)
+			printf(" ");
+		i++;
 	}
 	printf('\n');
 
