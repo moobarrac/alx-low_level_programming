675f203cf2d34cf16dab67bd2763c7813b901456
moobarrac <wahabajibola01@gmail.com>
2022-06-09 17:17:11 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,7 @@
 
 /**
  * main - main block
+ * @void: no arg
  * Return: 0
  */
 
@@ -19,7 +20,7 @@ int main(void)
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else
-			print("%d ",i);
+			print("%d", i);
 
 		if (i != 100)
 			printf(" ");
