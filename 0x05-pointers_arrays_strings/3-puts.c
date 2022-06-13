0f1642b8942496c4534a66b121847b93589c792f
moobarrac <wahabajibola01@gmail.com>
2022-06-13 07:34:01 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: char array type
+ * Description: only use _putchar
+ */
+
+int _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
