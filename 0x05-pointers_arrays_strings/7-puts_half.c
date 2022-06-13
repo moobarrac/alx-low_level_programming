c03a2b6b5a0e549c25d19ada41ad05c3992d6a2e
moobarrac <wahabajibola01@gmail.com>
2022-06-13 14:45:08 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of a str
+ * @str: char array str type
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
