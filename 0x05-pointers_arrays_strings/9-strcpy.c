08d925da3f6e99ef8cc5b786dbc3498d65d79aaf
moobarrac <wahabajibola01@gmail.com>
2022-06-13 15:10:43 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - cpoy string to pointer
+ * @dest: char type string
+ * @src: char type string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
