ccf45db9bb5e6882b253eab1098e5bfdd317add2
moobarrac <wahabajibola01@gmail.com>
2022-06-20 07:36:40 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	while (s != '\0')
+	while (*s != '\0')
 	{
-		if (s == c)
+		if (*s == c)
 		{
 			return (s);
 		}
-		else ((s + 1) == c)
+		else (*(s + 1) == c)
 		{
 			return (s + 1);
 		}
