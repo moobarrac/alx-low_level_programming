99a82d7ef485cc6fccedf45d253a582d526f09fd
moobarrac <wahabajibola01@gmail.com>
2022-06-22 06:58:54 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,25 +9,18 @@
 int _sqrt_recursion(int n)
 {
 	return (helper(n, 1));
-}
-
-/**
- * helper - help to solve the sqrt recursion
- * @s: number ot check for sqr root
- * @i: increment to compare
- * Return: sqr root or -1 if not
- */
 
 
-int helper(int s, int i)
-{
-	int sqr;
+	int helper(int s, int i)
+	{
+		int sqr;
 
-	sqr = i * i;
-	if (sqr == s)
-		return (i);
-	else if (sqr < s)
-		return (helper(s, i + 1));
-	else
-		return (-1);
+		sqr = i * i;
+		if (sqr == s)
+			return (i);
+		else if (sqr < s)
+			return (helper(s, i + 1));
+		else
+			return (-1);
+	}
 }
